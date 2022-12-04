b20c4b55d4f8eeb504d945232d6fd37d7bdfc8b0
Tim Fennis <tim.fennis@nos.nl>
2022-12-04 08:41:58 +0100

    compact code

--- python/day4/day4.py
+++ python/day4/day4.py
@@ -1,11 +1,8 @@
 input = open("input").read()
 
-full = 0
-partial = 0
+full, partial = 0, 0
 for line in input.split():
-    a, b = line.split(',')
-    a1,a2 = a.split('-')
-    b1,b2 = b.split('-')
+    [a1, a2], [b1, b2] = [it.split('-') for it in line.split(',')]
 
     r1 = set(range(int(a1), int(a2) + 1))
     r2 = set(range(int(b1), int(b2) + 1))
