4480795292395beed88f33c3049c11eed6d7d1b1
Tim Fennis <tim.fennis@nos.nl>
2022-12-06 08:24:44 +0100

    update rust solution

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -4,31 +4,15 @@ use itertools::Itertools;
 
 fn main() {
     let input = include_str!("../input");
-    println!("{}", find_start_sequence_alt(input, 4));
+    println!("{}", find_start_sequence(input, 4));
     println!("{}", find_start_sequence(input, 14));
 }
 
-/// Thieved from le reddit
-fn find_start_sequence_alt(sequqnce: &str, len: usize) -> usize {
+fn find_start_sequence(sequqnce: &str, len: usize) -> usize {
     return sequqnce
         .as_bytes()
         .windows(len)
-        .enumerate()
-        .find(|(_, w)| w.iter().collect::<HashSet<_>>().len() == len)
+        .find_position(|w| w.iter().collect::<HashSet<_>>().len() == len)
         .unwrap()
-        .0 + len;
-}
-
-fn find_start_sequence(sequence: &str, size: usize) -> usize {
-    for (idx, _) in sequence.chars().enumerate() {
-        if idx >= size {
-            let sub_seq: HashSet<char> = sequence[idx - size..idx].chars().collect::<_>();
-
-            if sub_seq.len() == size {
-                return idx;
-            }
-        }
-    }
-
-    return 0;
+        .0
 }
