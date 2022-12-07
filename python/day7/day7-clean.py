df28d83db96438c33cb0252e020dd7514e334db2
Tim Fennis <tim.fennis@nos.nl>
2022-12-07 09:11:03 +0100

    improve minimal version

--- python/day7/day7-clean.py
+++ python/day7/day7-clean.py
@@ -1,6 +1,4 @@
-input = open('input').read()
-lines = input.split('\n')
-
+lines = open('input').read().split('\n')
 directories = dict()
 current_dir = []
 
@@ -8,22 +6,18 @@ for line in lines:
     if line.startswith('$ cd'):
         _, _, dir_cmd = line.split(' ')
         if dir_cmd == '..':
-            assert len(current_dir) > 0
             current_dir.pop()
-        elif dir_cmd.isalpha or dir_cmd == '/':
+        else:
             current_dir.append(dir_cmd)
             directories['/'.join(current_dir)] = 0
-        else:
-            print("Unexpected dir", dir_cmd)
     elif line.startswith('$ ls'):
         continue
     elif line.startswith('dir'):
         continue
     else:
-        size, name = line.split(' ')
-        size = int(size)
+        size, _ = line.split(' ')
         for i in range(1, len(current_dir) + 1):
-            directories['/'.join(current_dir[:i])] += size
+            directories['/'.join(current_dir[:i])] += int(size)
 
 print(sum([size for size in directories.values() if size <= 100000]))
 
