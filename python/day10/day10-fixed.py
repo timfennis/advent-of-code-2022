3c39649c77cfaafcce0d02df5b4eb32c7611dc42
Tim Fennis <tim.fennis@nos.nl>
2022-12-10 07:06:42 +0100

    Fixed and cleaned day 10

--- python/day10/day10-fixed.py
+++ python/day10/day10-fixed.py
@@ -1,28 +1,20 @@
 lines = [ line.strip() for line in open('input').read().split("\n") ]
 
-cycle = 0
-x = 1
+cycle, x = 0, 1
 samples = []
 CRT = ['.' for _ in range(6 * 40)]
 
 def update(cycle, x):
-    if cycle > len(CRT):
-        return
+    cycle += 1
+    samples.append((cycle, x))
     if (cycle - 1) % 40 in [x - 1, x, x + 1]:
         CRT[cycle - 1] = '#'
-    else:
-        CRT[cycle - 1] = '.'
 
 for line in lines:
-    parts = line.split()
-    cycle += 1
     update(cycle, x)
-    samples.append((cycle, x))
-    if len(parts) == 2:
-        cycle += 1
+    if line.startswith('addx'):
         update(cycle, x)
-        samples.append((cycle, x))
-        x += int(parts[1])
+        x += int(line.split()[-1])
 
 print(sum([cycle * x for (cycle, x) in samples if (cycle + 20) % 40 == 0]))
 
